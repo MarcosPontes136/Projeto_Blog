b9d5e4bc6f227a6e8bc4c0e712b8fbfb29de4c8c
MarcosPontes136 <zeuscod@outlook.com>
2023-05-27 21:39:21 -0300

    Foi corrigido partes do layout e do estilo em geral.

--- src/app/feed/feed.component.ts
+++ src/app/feed/feed.component.ts
@@ -5,7 +5,7 @@ import { FeedService } from '../service/feed.service';
 @Component({
   selector: 'app-feed',
   templateUrl: './feed.component.html',
-  styleUrls: ['./feed.component.css']
+  styleUrls: ['./feed.component.scss']
 })
 export class FeedComponent implements OnInit {
 
