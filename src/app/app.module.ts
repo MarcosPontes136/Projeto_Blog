ceb36e5677312b2bb0c1b46db295024b062b01d1
MarcosPontes136 <zeuscod@outlook.com>
2023-06-18 20:27:22 -0300

    Criado um novo botão para subir ao topo do site.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,8 @@ import { FooterComponent } from './footer/footer.component';
 import { ContatoComponent } from './contato/contato.component';
 import { HttpClientModule } from '@angular/common/http'
 import { FeedComponent } from './feed/feed.component';
+import { MatIconModule } from '@angular/material/icon';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -27,6 +29,8 @@ import { FeedComponent } from './feed/feed.component';
     FontAwesomeModule,
     HttpClientModule,
     FormsModule,
+    MatIconModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
 import { faInstagram, faFacebook, faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
@@ -16,6 +16,23 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  @HostListener('window:scroll', []) onWindowScroll() {
+    this.funcaoRolagem();
+  }
+
+  funcaoRolagem() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+      document.getElementById('back-to-top')!.style.display = 'block';
+    } else {
+        document.getElementById('back-to-top')!.style.display = 'none';
+    }
+  }
+
+  voltarTopo() {
+      document.body.scrollTop = 0; // For Safari
+      document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
   }
 
 }
