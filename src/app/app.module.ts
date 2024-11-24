e425ba2f15788fb00fbebae19c152c992a449cb2
MarcosPontes136 <zeuscod@outlook.com>
2024-11-24 15:15:28 -0300

    Angular v17-v18 migration.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,30 +9,23 @@ import { FormsModule } from '@angular/forms';
 import { HomeComponent } from './home/home.component';
 import { FooterComponent } from './footer/footer.component';
 import { ContatoComponent } from './contato/contato.component';
-import { HttpClientModule } from '@angular/common/http'
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http'
 import { FeedComponent } from './feed/feed.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
 
-@NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    FooterComponent,
-    FeedComponent,
-    ContatoComponent,
-   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    FontAwesomeModule,
-    HttpClientModule,
-    FormsModule,
-    BrowserAnimationsModule,
-    MatIconModule
-  ],
-  providers: [MatIconRegistry],
-  bootstrap: [AppComponent]
-})
+@NgModule({ declarations: [
+        AppComponent,
+        NavbarComponent,
+        HomeComponent,
+        FooterComponent,
+        FeedComponent,
+        ContatoComponent,
+    ],
+    bootstrap: [AppComponent], imports: [BrowserModule,
+        AppRoutingModule,
+        FontAwesomeModule,
+        FormsModule,
+        BrowserAnimationsModule,
+        MatIconModule], providers: [MatIconRegistry, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
